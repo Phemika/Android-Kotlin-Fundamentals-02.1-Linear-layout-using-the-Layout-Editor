ea708bfe3d0763a6ca451f93de7e129b2b62d1f6
a <pamika.si@rmuti.ac.th>
2020-07-20 21:13:21 +0700

    02.2 Add user interactivity. 7 Task Add a click listener to the DONE button. Step1 Add a click listener

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+}
+private fun addNickname(view: View) {
+    val editText = findViewById<EditText>(R.id.nickname_edit)
+    val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+    nicknameTextView.text = editText.text
+    editText.visibility = View.GONE
+    view.visibility = View.GONE
+    nicknameTextView.visibility = View.VISIBLE
+
+
 }
