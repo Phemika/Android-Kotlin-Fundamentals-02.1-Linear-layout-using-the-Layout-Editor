ab90b8a7a99165f2f0d99132c2ff8f106e0c3d27
a <pamika.si@rmuti.ac.th>
2020-07-20 21:39:57 +0700

    02.2 Add user interactivity. 7 Task Add a click listener to the DONE button. Step3 Hide the keyboard

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.getSystemService
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -17,17 +20,20 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-}
 
 private fun addNickname(view: View) {
     val editText = findViewById<EditText>(R.id.nickname_edit)
     val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-   
+
     nicknameTextView.text = editText.text
     editText.visibility = View.GONE
     view.visibility = View.GONE
     nicknameTextView.visibility = View.VISIBLE
 
+    // Hide the keyboard.
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
 }
 
 
