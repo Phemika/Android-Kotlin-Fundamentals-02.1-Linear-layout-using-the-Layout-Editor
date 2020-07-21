b40a2583ad807ebb8bf12d761d2ac9d9515551f6
a <pamika.si@rmuti.ac.th>
2020-07-21 22:17:57 +0700

    02.4Data binding basics 4Task step1

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -22,6 +22,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
@@ -35,10 +39,12 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            nicknameText.text = nicknameEdit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val inputMethodManager =
